715efd68e35976dd7fe1cce546b1cca110954d5c
SunRise-11 <cleanpinesunrise03@gmail.com>
2021-09-07 19:41:23 +0100

    Add GetOrders Handler

--- store_mock_test.go
+++ store_mock_test.go
@@ -29,7 +29,7 @@ func TestGetMealsHandler(t *testing.T) {
 	}
 	recorder := httptest.NewRecorder()
 
-	hf := http.HandlerFunc(getMealHandler)
+	hf := http.HandlerFunc(mockGetMealHandler)
 
 	// Now, when the handler is called, it should call our mock store, instead of
 	// the actual one
@@ -58,6 +58,54 @@ func TestGetMealsHandler(t *testing.T) {
 	mockStore.AssertExpectations(t)
 }
 
+func TestGetOrdersHandler(t *testing.T) {
+	// Initialize the mock store
+	mockStore := InitMockStore()
+
+	/* Define the data that we want to return when the mocks `GetOrders` method is
+	called
+	Also, we expect it to be called only once
+	*/
+	mockStore.On("GetOrders").Return([]*Order{
+		{1, "Spaghetti", 10, "spag.jpg", 5},
+	}, nil).Once()
+
+	req, err := http.NewRequest("GET", "", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	recorder := httptest.NewRecorder()
+
+	hf := http.HandlerFunc(mockGetOrderHandler)
+
+	// Now, when the handler is called, it should call our mock store, instead of
+	// the actual one
+	hf.ServeHTTP(recorder, req)
+
+	if status := recorder.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := Order{1, "Spaghetti", 10, "spag.jpg", 5}
+	m := []Order{}
+	err = json.NewDecoder(recorder.Body).Decode(&m)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual := m[0]
+
+	if actual != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", actual, expected)
+	}
+
+	// the expectations that we defined in the `On` method are asserted here
+	mockStore.AssertExpectations(t)
+}
+
 func TestCreateOrderHandler(t *testing.T) {
 
 	mockStore := InitMockStore()
@@ -66,7 +114,7 @@ func TestCreateOrderHandler(t *testing.T) {
 	 We expect the first argument to the method to be the order struct
 	 defined below, and tell the mock to return a `nil` error
 	*/
-	mockStore.On("CreateOrders", &Order{"meatpizza $10"}).Return(nil)
+	mockStore.On("CreateOrders", &Order{0, "meatpizza", 5, "pizza.png", 5}).Return(nil)
 
 	form := newCreateOrderForm()
 	req, err := http.NewRequest("POST", "", bytes.NewBufferString(form.Encode()))
@@ -97,7 +145,8 @@ func TestCreateCustomerInfoHandler(t *testing.T) {
 	 We expect the first argument to the method to be the customers struct
 	 defined below, and tell the mock to return a `nil` error
 	*/
-	mockStore.On("CreateCustomer", &Customer{"Faruq", "Hidden Leaf"}).Return(nil)
+	mockStore.On("DeleteOrders").Return(nil)
+	mockStore.On("CreateCustomer", &Customer{"Faruq", "Hidden Leaf", 4, "Spaghetti", 22}).Return(nil)
 
 	form := newCreateCustomerForm()
 	req, err := http.NewRequest("POST", "", bytes.NewBufferString(form.Encode()))
@@ -122,13 +171,19 @@ func TestCreateCustomerInfoHandler(t *testing.T) {
 
 func newCreateOrderForm() *url.Values {
 	form := url.Values{}
-	form.Set("foodName", "meatpizza $10")
+	form.Set("meal", "meatpizza")
+	form.Set("price", "5")
+	form.Set("image", "pizza.png")
+	form.Set("PlateNumbers", "5")
 	return &form
 }
 
 func newCreateCustomerForm() *url.Values {
 	form := url.Values{}
-	form.Set("user", "Faruq")
+	form.Set("customer", "Faruq")
 	form.Set("address", "Hidden Leaf")
+	form.Set("plates", "4")
+	form.Set("meal", "Spaghetti")
+	form.Set("price", "22")
 	return &form
 }
