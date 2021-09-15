793d681df9abe1c61f31d64af677aa46fffdb0e5
SunRise-11 <cleanpinesunrise03@gmail.com>
2021-09-15 02:03:31 +0100

    Adjust navbar behavior

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -3,6 +3,7 @@ let body = document.querySelector("body");
 let navBar = document.querySelector(".nav");
 let menuBtn = document.querySelector(".navbar-toggler-icon");
 let cancelBtn = document.querySelector(".cancel-btn");
+
 menuBtn.onclick = function(){
   menuBtn.style.opacity = "0";
   menuBtn.style.pointerEvents = "none";
@@ -20,5 +21,8 @@ let navLinks = document.querySelectorAll(".navbar-nav li a");
 for (var i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener("click" , function() {
     navBar.classList.remove("show");
+    menuBtn.style.opacity = "1";
+    cancelBtn.style.opacity = "0";
+    menuBtn.style.pointerEvents = "auto";
   });
 }
