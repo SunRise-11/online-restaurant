e860f379f344b341f6d3f15489bd729d099d1bf2
SunRise-11 <cleanpinesunrise03@gmail.com>
2021-09-16 18:13:50 +0100

    Add form and toast javascript files

---
+++ static/js/toast.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    $( "form" ).on( "submit", function(e) {
+           var dataString = $(this).serialize();
+          //  alert(dataString); return false;
+
+           $.ajax({
+              type: 'POST',
+              url: '/order',
+              data: dataString,
+              success: function (response) {
+                  $('.toast').toast('show');
+              }
+       });
+
+      e.preventDefault();
+    });
+  });
